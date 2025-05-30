wizardsardine/liana-retailer

gui: add message to open url in browser

gui/src/app/mod.rs
@@ -300,6 +300,12 @@ impl App {
                 )
             }
             Message::View(view::Message::Menu(menu)) => self.set_current_panel(menu),
+            Message::View(view::Message::OpenUrl(url)) => {
+                if let Err(e) = open::that_detached(&url) {
+                    tracing::error!("Error opening '{}': {}", url, e);
+                }
+                Command::none()
+            }
             Message::View(view::Message::Clipboard(text)) => clipboard::write(text),
             _ => self
                 .panels

jp1ac4/liana

liana-gui/src/app/mod.rs
@@ -384,6 +384,12 @@ impl App {
                 )
             }
             Message::View(view::Message::Menu(menu)) => self.set_current_panel(menu),
+            Message::View(view::Message::OpenUrl(url)) => {
+                if let Err(e) = open::that_detached(&url) {
+                    tracing::error!("Error opening '{}': {}", url, e);
+                }
+                Task::none()
+            }
             Message::View(view::Message::Clipboard(text)) => clipboard::write(text),
             _ => self
                 .panels